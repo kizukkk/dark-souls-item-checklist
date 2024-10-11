644961818c80d7d08fe6f935a92ba3ec4fc036f9
kizukkk <zenro934@gmail.com>
2024-10-11 23:08:54 +0300

    fix: import module

--- assets/scripts/data.js
+++ assets/scripts/data.js
@@ -1,6 +1,8 @@
 import  {parsItemsAsync} from './parser.js';
 import {writeFile} from 'fs';
+import { readdir, readFile} from 'node:fs/promises';
 import fs from "fs";
+import path from 'path';
 
 const PATH = './assets/data'
 
